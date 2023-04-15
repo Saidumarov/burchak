b2c8706a487494dc3e13bf9eaee70b07449b0c99
Saidumarov <saidumarovjafarxon@gmail.com>
2023-04-15 23:43:05 +0500

    new commit

--- bur.js
+++ bur.js
@@ -7,11 +7,11 @@ const c = Number(prompt("Uchinchi sonni kiriting: "))
 
 if (a + b > c && b + c > a && a + c > b) {
     if (a == b && b == c) {
-        document.querySelector(".ok").textContent=("Uchburchak teng tomonli teng yonli.");
+        document.querySelector(".ok").textContent=(" Teng tomonli uchburchak.");
     } else if (a == b || b == c || c == a) {
-        document.querySelector(".ok").textContent=("Uchburchak teng yonli.");
+        document.querySelector(".ok").textContent=(" Teng yonli uchburchak.");
     } else {
-        document.querySelector(".ok").textContent=("Uchburchak o'zaro qarama-qarshi tomonli.");
+        document.querySelector(".ok").textContent=("O'zaro qarama-qarshi tomonli uchburchak .");
     }
 } else {
     document.querySelector(".ok").textContent=("Uchburchak kiritilmagan, yoki kiritilgan sonlar uchburchakni yaratmaydi.");
